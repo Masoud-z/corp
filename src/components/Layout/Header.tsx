477e7d422bd5f9f72470c092c1f302f94ad4bce0
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-21 14:57:17 +0330

    give style to header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-    </div>
+    <nav className="flex absolute text-white mx-auto w-full justify-between items-center p-8 container z-10">
+      <Link href="/" className="font-bold text-3xl">
+        Home
+      </Link>
+      <div className="text-xl space-x-4">
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+    </nav>
   );
 };
 
